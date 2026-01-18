b19eb66ed9157922ee972a042ef5bc130bf60dd2
mariuszf <marius@fink.no>
2026-01-18 15:58:10 +0100

    New changes to deployment for production

--- backend/config/config.js
+++ backend/config/config.js
@@ -11,13 +11,13 @@ module.exports = {
   userGameUUIDPrefix: "usergame---",
   serverFullUrl: debug
     ? "http://localhost:1337/"
-    : "https://character-picker-backend.herokuapp.com/",
+    : "http://192.168.1.13:3003/api/",
   cacheForMinutes: 1000 * 60 * 180,
   emailKey: process.env.EMAIL_KEY,
   emailFrom: process.env.EMAIL_FROM,
   emailLandingPagePasswordReset: debug
     ? "http://localhost:8080/#/password-reset/"
-    : "http://www.randomboardgame.com/#/password-reset/",
+    : "http://192.168.1.13:3003/#/password-reset/",
   adminAuthUser: process.env.ADMIN_AUTH_USER,
   adminAuthPass: process.env.ADMIN_AUTH_PASS,
   adminAuthUser2: process.env.ADMIN_AUTH_USER2,
